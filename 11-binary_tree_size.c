5fe6cdded14258ab098b58156fc543ce101de62c
AgaiMorara <corneliusmorara@gmail.com>
2023-11-02 15:41:11 -0700

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,9 +6,10 @@
  *Return: total number of nodes in the tree
  */
 
-size)t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+	return (1 + binary_tree_size(tree->left)
+		+ binary_tree_size(tree->right));
 }
