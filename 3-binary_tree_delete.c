b09cafa5279de23f2ceab5903a87114016367003
AgaiMorara <corneliusmorara@gmail.com>
2023-11-02 11:00:50 -0700

    recursion is key

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,12 +3,12 @@
 
 /**
  *binary_tree_delete - deletes all the nodes in a binary tree
+ *@tree: pointer to the tree to be deleted.
  *if tree is null, does nothing
  */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	{
 	if (tree != NULL)
 	{
 		binary_tree_delete(tree->left);
