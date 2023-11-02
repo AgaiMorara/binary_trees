360dddc6c9e43f4d77d3192e253da74bbddd73ad
AgaiMorara <corneliusmorara@gmail.com>
2023-11-02 06:21:17 -0700

    insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,7 +7,7 @@
  *the old left-child must be set as the left child of the new node.
  *@parent: pointer to the node to insert the left-child
  *@value: value to store in the new node
- *Returns: pointer to created node, Null if failure
+ *Return: pointer to created node, Null if failure
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
@@ -18,11 +18,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	ans = malloc(sizeof(binary_tree_t));
 	if (!ans)
 		return (NULL);
-	if(parent->left)
+	if (parent->left)
 	{
 		temp = parent->left;
 		parent->left = ans;
 		ans->left = temp;
+		ans->left->parent = ans;
 	}
 	else
 		parent->left = ans;
