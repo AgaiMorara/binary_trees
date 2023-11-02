93ca3c6bfec5d3fc54bae40e02c8688454d22598
AgaiMorara <corneliusmorara@gmail.com>
2023-11-02 11:36:22 -0700

    tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,14 +11,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_left, height_right;
 
 
-	if (!tree)
+	if (!tree || (!tree->left && !tree->right))
 		return (0);
 
 	height_left = binary_tree_height(tree->left);
 	height_right = binary_tree_height(tree->right);
 
-	if (height_left > height_right)
-		return (height_left + 1);
-	else
-		return (height_right + 1;
+	return (1 + (height_left > height_right ? height_left : height_right));
 }
