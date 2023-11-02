860778bb49107091625d64f0260fa4f5eae1d850
AgaiMorara <corneliusmorara@gmail.com>
2023-11-02 06:43:32 -0700

    insert left correctly

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,38 +2,35 @@
 #include <stdlib.h>
 
 /**
- *binary_tree_insert_left - inserts a node as the left-child of another
- *If parent already has a left-child, the new node must take its place, and
- *the old left-child must be set as the left child of the new node.
- *@parent: pointer to the node to insert the left-child
- *@value: value to store in the new node
- *Return: pointer to created node, Null if failure
+ * binary_tree_insert_left - Inserts a node as the left child of another.
+ * If parent already has a left child, the new node takes its place, and
+ * the old left child becomes the left child of the new node.
+ * @parent: Pointer to the node to insert the left child.
+ * @value: Value to store in the new node.
+ * Return: Pointer to the created node, NULL if failure.
  */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *ans;
-	binary_tree_t *temp;
+	binary_tree_t *ans = malloc(sizeof(binary_tree_t));
 
-	ans = malloc(sizeof(binary_tree_t));
+	if (!parent)
+		return (NULL);
+
 	if (!ans)
 		return (NULL);
-	if (parent && parent->left)
+
+	ans->n = value;
+	ans->parent = parent;
+	ans->left = NULL;
+	ans->right = NULL;
+
+	if (parent->left)
 	{
-		temp = parent->left;
-		parent->left = ans;
-		ans->left = temp;
-		ans->left->parent = ans;
+		ans->left = parent->left;
+		parent->left->parent = ans;
 	}
-	else
-	{
-		if (!parent)
-			return (NULL);
-		parent->left = ans;
-	}
-/* allocate remaining slots of ans */
-		ans->right = NULL;
-		ans->parent = parent;
-		ans->n = value;
-		return (ans);
+
+	parent->left = ans;
+
+	return (ans);
 }
