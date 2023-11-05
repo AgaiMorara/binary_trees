092a7f7355cc61c4e9085000fea76e09230db9f6
AgaiMorara <corneliusmorara@gmail.com>
2023-11-05 02:56:32 -0800

    traverse up each step

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,53 +1,18 @@
 #include "binary_trees.h"
-/**
- *binary_trees_ancestor - finds the lowest common ancestor of two nodes
- *@first - pointer to the first node
- *@second - pointer to the second node
- *Returh: pointer to the lowest common ancestor
- */
 
-binary_tree_t *binary_tree_ancestor(const binary_tree_t *first,
-				    const binary_tee_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
 {
-	const binary_tree_t *temp;
-	size_t depth_f = 0, depth_s = 0;
+	const binary_tree_t *f = first, *s = second;
 
 	if (!first || !second)
 		return (NULL);
-	if (first == second)
-		return ((binary_tree_t *) first);
-	temp = first;
 
-	while (temp)
+	while (f != s)
 	{
-		depth_f ++;
-		temp = temp->parent;
+		f = f ? f->parent : second;
+		s = s ? s->parent : first;
 	}
 
-	temp = second;
-	while (temp)
-	{
-		depth_s++;
-		temp = temp->parent;
-	}
-
-	while(depth_f > depth_s)
-	{
-		first = first->parent;
-		depth_first--;
-	}
-
-	while (depth_s > depth_f)
-	{
-		second = second->parent;
-		depth_second--;
-	}
-
-	while (first != second)
-	{
-		first = first->parent;
-		second = second->parent;
-	}
-
-	return (binary_tree_t *) first;
+	return (binary_tree_t *)f;
 }
